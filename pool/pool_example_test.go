a593e843e54bd80fb9ad2ec67e6f7a475476b081
Sam <copperboat@hey.com>
2024-05-23 19:46:41 -0500

    Pool: add examples

---
+++ pool/pool_example_test.go
@@ -0,0 +1,95 @@
+package pool_test
+
+import (
+	"fmt"
+
+	"github.com/green-aloe/utility/pool"
+)
+
+func ExamplePool_Get() {
+	type Person struct {
+		Name string
+		Age  int
+
+		internal int
+	}
+
+	pool := pool.Pool[Person]{
+		NewItem: func() Person {
+			return Person{
+				Name: "Alice",
+				Age:  74,
+			}
+		},
+	}
+
+	person := pool.Get()
+	fmt.Println(person)
+
+	// Output:
+	// {Alice 74 0}
+}
+
+func ExamplePool_Store() {
+	type Person struct {
+		Name string
+		Age  int
+
+		internal int
+	}
+
+	pool := pool.Pool[Person]{
+		NewItem: func() Person {
+			return Person{
+				Name: "Alice",
+				Age:  74,
+			}
+		},
+		ClearItem: func(person Person) Person {
+			person.Name = ""
+			person.Age = 0
+
+			return person
+		},
+	}
+
+	pool.Store(Person{
+		Name:     "Bob",
+		Age:      42,
+		internal: 1,
+	})
+
+	person := pool.Get()
+	fmt.Println(person)
+
+	// Output:
+	// { 0 1}
+}
+
+func ExamplePool_Count() {
+	var pool pool.Pool[int]
+	for i := 0; i < 10; i++ {
+		pool.Store(i)
+	}
+
+	count := pool.Count()
+	fmt.Println(count)
+
+	// Output:
+	// 10
+}
+
+func ExamplePool_Clear() {
+	var pool pool.Pool[int]
+	for i := 0; i < 10; i++ {
+		pool.Store(i)
+	}
+
+	pool.Clear()
+
+	count := pool.Count()
+	fmt.Println(count)
+
+	// Output:
+	// 0
+}
